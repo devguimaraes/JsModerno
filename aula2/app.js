44e378f84ef55b76d8a87f11e9f0d96c9e677ad2
Bruno Casa <devgmrs@gmail.com>
2020-02-04 21:32:37 -0300

    Aulas de comentários JS

---
+++ aula2/app.js
@@ -0,0 +1,14 @@
+/*
+        Comentários JS
+
+Comentários são trecho de códigos, ignorado pelo compilador
+*/
+
+console.log("Linha 1");
+//comentário
+console.log("Linha 2");
+//comentário
+console.log("Linha 3");
+//comentário
+console.log("Linha 4");
+//Comentários são ignorados em qualquer trecho. Esse é o comentário
