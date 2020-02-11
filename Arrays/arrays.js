56f6d3c487158d5fd152ca6df4f91cadb6a87577
Bruno Casa <devgmrs@gmail.com>
2020-02-10 21:17:59 -0300

    Aula de Array

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,1 +1,25 @@
 //Aula sobre Arrays
+
+//Array é uma coleção de coisas
+let alunos = ["Ana", "Rita", "Joana", "Iracema", "Carolina"];
+
+delete alunos[1] //Apagar um item especifico. Rita 
+
+
+//alunos[1] = "Rita"; //Adicionando ao indíce 1 - Rita
+
+
+//console.log(alunos instanceof Array); //Verifica se é uma rray
+
+//const removidos = alunos.pop();//Remove um item do final do array;
+
+// const removidos2 = alunos.shift()//Remove um item do começo do array
+
+
+// alunos[1] = "Iracema"; //Adiciona item ao array
+// alunos[3] = "Rita";
+// alunos[5] = "Ana Carolina";
+
+//alunos.push("Seu Jorge");//Adiciona um item ao final do array
+
+//alunos.unshift("Ana Claudia");//Adiocina um item ao começo do array
