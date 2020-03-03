bf448b95fc09407a0dd04f473d08072769bd376d
Bruno Casa <devgmrs@gmail.com>
2020-03-03 20:46:49 -0300

    Exercicios finais de lógica JS

---
+++ exerciciosLOGICAFINAL/exec.js
@@ -0,0 +1,12 @@
+//faça um função que receba dois parametros é retorne o maior
+
+// function retornaMaior(num1, num2){
+//    return (num1 > num2 ? num1 : num2)
+// }
+
+
+const max2 = (x, y) => {
+    return (x > y ? x : y)
+}
+
+console.log(max2(5,6))
