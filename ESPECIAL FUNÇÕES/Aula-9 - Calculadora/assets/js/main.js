a8e2e4484e364afeb74a872d6af172cb3850d0e3
Bruno Casa <devgmrs@gmail.com>
2020-03-19 04:05:07 -0300

    Exercicios de função construtora com Calculadora

---
+++ ESPECIAL FUNÇÕES/Aula-9 - Calculadora/assets/js/main.js
@@ -0,0 +1,63 @@
+function criarCalculadora() {
+    return {
+        display: document.querySelector('.displayInput'),
+
+        inicia() {
+            this.cliqueBotao()
+           
+        },
+        
+
+        clearDisplay(){
+            this.display.value = '';
+        },
+
+        apagaUm(){
+            this.display.value = this.display.value.slice(0,-1);
+        },
+
+        realizaConta(){
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta) 
+                this.display.value = conta;
+            } catch (e) {
+                this.display.classList.add('resposta')
+                this.display.value = 'Está não é uma conta válida';
+            }
+
+        },
+
+
+        cliqueBotao() {
+            document.addEventListener('click', (e) => {
+                const el = e.target;
+
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if(el.classList.contains('btn-clear')){
+                    this.clearDisplay();
+                }
+
+                if(el.classList.contains('btn-apagar')){
+                    this.apagaUm()
+                }
+
+                if(el.classList.contains('btn-igual')){
+                    this.realizaConta()
+                }
+
+            })
+        },
+        btnParaDisplay(valor){
+            this.display.value += valor;
+        },
+    }
+        
+}
+
+const calculadora = criarCalculadora();
+calculadora.inicia()
